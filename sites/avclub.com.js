eb07532e41e2efb4e7ef34cebfd36ff6f8a4217d
Sean McAfee <eefacm@gmail.com>
2014-11-02 13:09:20 -0800

    Formatting tweaks.

--- sites/avclub.com.js
+++ sites/avclub.com.js
@@ -1,11 +1,15 @@
 //  Eliminate cruft.
 
-const body = $("div.article-body");
-body.children("section.article-text").nextAll().remove();
-body.nextAll("aside.article-side").remove();
+if (buffer.current_uri.path.length > 2) {
+    const body = $("div.article-body");
+    body.children("section.article-text").nextAll().remove();
+    body.nextAll("aside.article-side").remove();
 
-$("div#comment-ad").remove();
-$("div.advertisement.top-banner").remove();
-$("div#dfp-ad-wallpaper").remove();
-$("div.header-container").remove();
-$(".taboola").remove();
+    $("div#comment-ad").remove();
+    $("div.advertisement.top-banner").remove();
+    $("div#dfp-ad-wallpaper").remove();
+    $("div.header-container").remove();
+    $(".taboola").remove();
+
+    autoload_disqus_comments(1000);
+}
