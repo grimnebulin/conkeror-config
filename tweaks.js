39f251bd501d82e6412548dff98a343d71e1c97d
Sean McAfee <eefacm@gmail.com>
2014-11-02 13:05:20 -0800

    Customization of save-file destination.

--- tweaks.js
+++ tweaks.js
@@ -56,3 +56,76 @@ interactive("bookmark",
         add_bookmark(uri_string, title);
         I.minibuffer.message("Added bookmark: " + uri_string + " - " + title);
     });
+
+interactive("save",
+    "Save a browser object.",
+    function (I) {
+        var element = yield read_browser_object(I);
+        var spec = load_spec(element);
+        var panel;
+        panel = create_info_panel(I.window, "download-panel",
+                                  [["downloading",
+                                    element_get_operation_label(element, "Saving"),
+                                    load_spec_uri_string(spec)],
+                                   ["mime-type", "Mime type:", load_spec_mime_type(spec)]]);
+        try {
+            var file = yield I.minibuffer.read_file_check_overwrite(
+                $prompt = "Save as:",
+                $initial_value = suggest_save_path(spec, I.buffer),
+                $history = "save");
+        } finally {
+            panel.destroy();
+        }
+        save_uri(spec, file,
+                 $buffer = I.buffer,
+                 $use_cache = false);
+    },
+    $browser_object = browser_object_links);
+
+function suggest_save_path(spec, buffer) {
+    const wrapper = spec_wrapper(spec);
+    wrapper.page_title = buffer.document.title;
+
+    let file_name = wrapper.filename;
+    if (! file_name) {
+        file_name = generate_filename_safely_fn(
+            maybe_filename_from_content_disposition(spec) ||
+            maybe_filename_from_title(spec) ||
+            maybe_filename_from_uri(spec) ||
+            maybe_filename_from_url_last_directory(spec) ||
+            maybe_filename_from_url_host(spec) ||
+            maybe_filename_from_localization_default() ||
+            "index");
+    }
+    const extension = maybe(file_name.match(/(\.[^.]*)$/))
+        .map(x => x[1])
+        .getOrElse("");
+    const dest = {
+        dir: undefined,
+        filename: file_name.substr(0, file_name.length - extension.length),
+        extension: extension
+    };
+    const host = buffer.current_uri.host;
+    for (let entry of save_funcs) {
+        if (entry.length == 1 ||
+            entry.length == 2 && (host == entry[0] ||
+                                  host.endsWith("." + entry[0]))) {
+            try {
+                const record = entry[entry.length - 1](wrapper);
+                if (record)
+                    for (let [key, value] of Iterator(record))
+                        if (key in dest && value)
+                            dest[key] = value;
+            } catch (e) { dumpln(e) }
+        }
+    }
+    const dir = dest.dir === undefined
+        ? get_home_directory().path
+        : dest.dir.startsWith("/")
+        ? dest.dir
+        : let (p = get_home_directory())
+              (p.appendRelativePath(dest.dir), p.path);
+    return dir.replace(/\/*$/, "/")
+        + dest.filename.replace(/\//g, "-")
+        + dest.extension.replace(/^\.?([^.])/, ".$1");
+}
