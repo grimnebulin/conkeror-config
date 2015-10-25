0a0242fec5fed56ea509b6ea8a0a2d3971f721d9
Sean McAfee <eefacm@gmail.com>
2015-10-25 14:48:03 -0700

    Add a new site to be autokilled.

--- config.js
+++ config.js
@@ -230,7 +230,7 @@ function next_page(I) {
 define_key(default_global_keymap, "C-.", next_page);
 
 on_dom_loaded(
-    /cdn\.optmd\.com|axp\.zedo\.com|media\.fastclick\.net|adrotator\.se|voicefive\.com|tribalfusion\.com|aproductmsg\.com|productmsg\.com/,
+    /cdn\.optmd\.com|axp\.zedo\.com|media\.fastclick\.net|adrotator\.se|voicefive\.com|tribalfusion\.com|aproductmsg\.com|productmsg\.com|timelypayments\.com/,
     kill_buffer,
     true
 );
