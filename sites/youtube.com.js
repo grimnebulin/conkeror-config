cc2ccd5b28d9779f48288771a1798cabe36e7a3e
Sean McAfee <eefacm@gmail.com>
2017-02-06 14:49:01 -0800

    Fix youtube code

--- sites/youtube.com.js
+++ sites/youtube.com.js
@@ -1,6 +1,7 @@
 "use strict";
 
 $.whenFound("div.pyv-afc-ads-container", remove_it);
+
 //
 //  This code inserts hyperlinks on a YouTube video page to all of the
 //  raw video files available on that page.
@@ -17,25 +18,13 @@ if (match) {
 
 const vars_of = function (str) {
     return str.split(/[&,]/).map(p => p.split(/=/).map(decodeURIComponent));
-    // return Array.from(
-    //     param.split(/=/).map(decodeURIComponent)
-    //     for (param of str.split(/[&,]/))  // Comma is a separator too?!
-    // );                                    // Well-played, Google.
 };
 
 const object_from = function (src) {
     const obj = { };
-    while (true) {
-        const x = src.next();
-        if (x.done) {
-            break;
-        } else {
-            obj[x.value[0]] = x.value[1];
-        }
+    for (let [key, value] of src) {
+        obj[key] = value;
     }
-    // for (let [key, value] of src) {
-    //     obj[key] = value;
-    // }
     return obj;
 };
 
@@ -67,7 +56,6 @@ function yt_info_callback(str) {
     }
     const format = object_from(
         flashvars.fmt_list.split(/,/).map(x => x.split(/\//))
-        // x.split(/\//) for (x of flashvars.fmt_list.split(/,/))
     );
     const vids = vars_of(flashvars.url_encoded_fmt_stream_map)
           .reduce(function (acc, [key, value]) {
