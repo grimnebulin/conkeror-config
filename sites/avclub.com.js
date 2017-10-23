48b5e7e995415761f0ee009156b3069f9cb0a33d
Sean McAfee <eefacm@gmail.com>
2017-10-22 23:39:48 -0700

    AV Club: start over from scratch

--- sites/avclub.com.js
+++ sites/avclub.com.js
@@ -2,19 +2,7 @@
 
 //  Eliminate cruft.
 
-if (buffer.current_uri.path.length > 2) {
-    const body = $("div.article-body");
-    body.nextAll("aside.article-side").remove();
-
-    $("div#comment-ad").remove();
-    $("div.advertisement.top-banner").remove();
-    $("div#dfp-ad-wallpaper").remove();
-    $("div.header-container").remove();
-    $(".taboola").remove();
-
-    $("a.see-comments").clickthis();
-    $("div[class*='amzn-']").remove();
-    $("div.header-ad-wrapper").remove();
-
-    autoload_disqus_comments(1000);
-}
+$.onDocumentMutation(function () {
+    $("div[class*='recent-video__video-container']").closest("div.instream-native-video").remove();
+    $("div.ad-unit").remove();
+});
