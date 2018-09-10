dc870c26b54c88a6f248068cb309acb729ab3567
Sean McAfee <eefacm@gmail.com>
2018-09-09 17:16:59 -0700

    Add comics webjump

--- config.js
+++ config.js
@@ -61,6 +61,7 @@ define_webjump("sd", "http://www.scala-lang.org/api/current/index.html#package")
 define_webjump("ebay", "https://www.ebay.com/sch/i.html?_nkw=%s");
 define_webjump("tm", "http://www.thronemaster.net/?goto=gaming&sub=my_games");
 define_webjump("see", "https://www.justwatch.com/us/search?q=%s");
+define_webjump("comics", "https://customer.comichub.com/");
 
 define_webjump(
     "cx",
