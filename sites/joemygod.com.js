843c154ea5c4f61495f117515d83f3f6dfe32916
Sean McAfee <eefacm@gmail.com>
2019-08-25 13:53:23 -0700

    Remove Disqus ads more thoroughly

--- sites/joemygod.com.js
+++ sites/joemygod.com.js
@@ -5,6 +5,7 @@ autoload_disqus_comments();
 $.onDocumentMutation(function () {
     $("nav").remove();
     $("div._cm-os-slider").remove();
+    $("#disqus_thread > iframe[src*='/ads-iframe/']").remove();
 });
 
 $.whenFound("#taboola-below-article-thumbnails", remove_it);
